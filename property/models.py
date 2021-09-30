ba276b597e6c548f8597b41e444426f802796ae5
Yulia51188 <sun51188@gmail.com>
2021-09-30 11:59:52 +0300

    Make likes_by field optional with blank=True

--- property/models.py
+++ property/models.py
@@ -55,7 +55,7 @@ class Flat(models.Model):
         db_index=True)
 
     liked_by = models.ManyToManyField(User, related_name='liked_flats',
-        verbose_name='Понравилась пользователям')
+        verbose_name='Понравилась пользователям', blank=True)
 
     def __str__(self):
         return f'{self.town}, {self.address} ({self.price}р.)'
